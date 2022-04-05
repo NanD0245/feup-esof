bc44d5e1368613d9ea7a86621b0e5710a52b38a6
Bruno Mendes <brod56@sapo.pt>
2022-04-05 18:46:52 +0100

    Implement bottom navigation bar

---
+++ sigarra_eye_candy/lib/view/pages/materials.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MaterialsPage extends StatelessWidget {
+  const MaterialsPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: const Color(0xffC4DFCB),
+      child: Center(
+        child: Text(
+          "MaterialsPage",
+          style: TextStyle(
+            color: Colors.green[900],
+            fontSize: 45,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+}
