1a4ea2471b1d16e23ffaadb82dfd1b7dc43d286b
Bruno Mendes <bd_mendes@outlook.com>
2022-05-01 16:22:45 +0100

    Fetch course unit sheets from store

--- app_feup/lib/view/Widgets/course_units/course_units_sheets_view.dart
+++ app_feup/lib/view/Widgets/course_units/course_units_sheets_view.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:tuple/tuple.dart';
 import 'package:uni/model/entities/course_unit_sheet.dart';
 
+import '../../../model/app_state.dart';
+import '../request_dependent_widget_builder.dart';
 import 'course_unit_sheet_card.dart';
 
 class CourseUnitsSheetsView extends StatefulWidget {
@@ -9,37 +13,54 @@ class CourseUnitsSheetsView extends StatefulWidget {
 }
 
 class _CourseUnitsSheetsViewState extends State<StatefulWidget> {
-  final List<CourseUnitSheet> _courses = [];
-
   @override
   Widget build(BuildContext context) {
-    final List<Widget> activeCourseCards = [];
-    final List<Widget> pastCourseCards = [];
-    _courses.forEach((courseSheet) {
-      if (courseSheet.active) {
-        activeCourseCards.add(CourseUnitSheetCard(courseSheet));
-      } else {
-        pastCourseCards.add(CourseUnitSheetCard(courseSheet));
-      }
-    });
-    return ListView(
-        children: [
-      Container(
-          padding: EdgeInsets.fromLTRB(30, 20, 0, 5),
-          child: Text(
-            'Semestre Atual',
-            textScaleFactor: 0.8,
-          )),
-    ]
-          ..addAll(activeCourseCards)
-          ..addAll([
-            Container(
-                padding: EdgeInsets.fromLTRB(30, 20, 0, 5),
-                child: Text(
-                  'Semestres Anteriores',
-                  textScaleFactor: 0.8,
-                )),
-          ])
-          ..addAll(pastCourseCards));
+    return StoreConnector<AppState,
+            Tuple2<List<CourseUnitSheet>, RequestStatus>>(
+        builder: (context, courseUnitsSheetsData) {
+          final courseUnitsSheets = courseUnitsSheetsData.item1;
+          final courseUnitsSheetsStatus = courseUnitsSheetsData.item2;
+          return RequestDependentWidgetBuilder(
+            context: context,
+            onNullContent: Center(
+                child:
+                    Text('Impossível obter fichas das unidades curriculares')),
+            status: courseUnitsSheetsStatus,
+            content: courseUnitsSheets,
+            contentChecker: courseUnitsSheets.isNotEmpty,
+            contentGenerator: (dynamic courseUnitSheets, BuildContext context) {
+              final List<Widget> activeCourseCards = [];
+              final List<Widget> pastCourseCards = [];
+              courseUnitsSheets.forEach((courseUnitSheet) {
+                if (courseUnitSheet.active) {
+                  activeCourseCards.add(CourseUnitSheetCard(courseUnitSheet));
+                } else {
+                  pastCourseCards.add(CourseUnitSheetCard(courseUnitSheet));
+                }
+              });
+              return ListView(
+                  children: [
+                Container(
+                    padding: EdgeInsets.fromLTRB(30, 20, 0, 5),
+                    child: Text(
+                      'Semestre Atual',
+                      textScaleFactor: 0.8,
+                    )),
+              ]
+                    ..addAll(activeCourseCards)
+                    ..addAll([
+                      Container(
+                          padding: EdgeInsets.fromLTRB(30, 20, 0, 5),
+                          child: Text(
+                            'Semestres Anteriores',
+                            textScaleFactor: 0.8,
+                          )),
+                    ])
+                    ..addAll(pastCourseCards));
+            },
+          );
+        },
+        converter: (store) => Tuple2(store.state.content['ucsSheets'],
+            store.state.content['ucsSheetsStatus']));
   }
 }
