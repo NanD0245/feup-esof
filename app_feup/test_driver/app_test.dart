6304dc51e0ed0c6b29cc41bab4340347b5a99916
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-05-08 23:10:25 +0100

    Remove unneeded features from acceptance tests

--- app_feup/test_driver/app_test.dart
+++ app_feup/test_driver/app_test.dart
@@ -3,20 +3,23 @@ import 'dart:async';
 import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
 
+import 'steps/be_on_page_step.dart';
+import 'steps/be_on_tab_step.dart';
+import 'steps/enrolled_at_least_n_course_step.dart';
 import 'steps/tap_button_n_times_step.dart';
 
 Future<void> main() {
   final config = FlutterTestConfiguration()
-    ..features = [RegExp('features/*.*.feature')]
+    ..features = [RegExp('test_driver/features/*.*.feature')]
     ..reporters = [
       ProgressReporter(),
       TestRunSummaryReporter(),
       JsonReporter(path: './report.json')
     ] // you can include the "StdoutReporter()" without the message level parameter for verbose log information
-    ..stepDefinitions = [TapButtonNTimesStep()]
+    ..stepDefinitions = [EnrolledAtLeastNCourseStep()]
     ..customStepParameterDefinitions = []
     ..restartAppBetweenScenarios = true
-    ..targetAppPath = "test_driver/app.dart";
+    ..targetAppPath = 'test_driver/app.dart';
   // ..tagExpression = "@smoke" // uncomment to see an example of running scenarios based on tag expressions
   return GherkinRunner().execute(config);
 }
