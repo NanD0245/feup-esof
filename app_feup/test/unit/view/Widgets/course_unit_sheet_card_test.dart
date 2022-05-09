9006a9c9c7b1df4276516b9b14813dc7f5accda0
retchut <retchut@protonmail.ch>
2022-05-09 23:59:24 +0100

    feat: add simple course sheet unit test

---
+++ app_feup/test/unit/view/Widgets/course_unit_sheet_card_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import '../../../testable_widget.dart';
+import 'package:uni/view/Widgets/course_units/course_unit_sheet_card.dart';
+import 'package:uni/model/entities/course_unit_sheet.dart';
+import 'package:uni/model/entities/course_unit_teacher.dart';
+import 'package:uni/model/entities/course_unit_evaluation_component.dart';
+
+void main(){
+  final String mockName = 'Name';
+  final String mockGoals = 'Goals';
+  final String mockProgram = 'Program';
+  final bool mockActive = true;
+  final List<CourseUnitTeacher> mockTeachers = [
+    CourseUnitTeacher('Teacher1', 'LectureType1', 'Hours1'),
+    CourseUnitTeacher('Teacher2', 'LectureType2', 'Hours2')
+  ];
+  final List<CourseUnitEvaluationComponent> evaluationComponents = [
+      CourseUnitEvaluationComponent('Designation1', 'Weight1'),
+      CourseUnitEvaluationComponent('Designation2', 'Weight2')
+  ];
+
+  final mockCourseUnitSheet = CourseUnitSheet(mockName, mockGoals, mockProgram,
+      evaluationComponents, mockTeachers, mockActive);
+
+    testWidgets('When given a simple Unit Sheet', (WidgetTester tester) async {
+      final widget = makeTestableWidget(
+        child: CourseUnitSheetCard(mockCourseUnitSheet)
+      );
+
+      await tester.pumpWidget(widget);
+
+      final textName = find.text('Name');
+      expect(textName, findsOneWidget);
+    });
+}
